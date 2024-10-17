090cc90001898eb124e346931c9819c89f2697db
elvenvdk <vanderkuech@icloud.com>
2024-10-17 15:16:18 -0400

    Added subgraph, auth.context, updatged current-user.decorator

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,28 +1,58 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { LoggerModule } from '@app/common';
+import { AUTH_SERVICE, LoggerModule } from '@app/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
-import { IntrospectAndCompose } from '@apollo/gateway';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { authContext } from './auth.context';
 
 @Module({
   imports: [
     GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
       useFactory: (configSerivce: ConfigService) => ({
+        server: {
+          context: authContext,
+        },
         gateway: {
           supergraphSdl: new IntrospectAndCompose({
             subgraphs: [
               {
                 name: 'reservations',
-                url: configSerivce.getOrThrow('RESERVATIONS_GRAPHQL_URL'),
+                url: configSerivce.getOrThrow(
+                  'http://reservations:3000/graphql',
+                ),
               },
             ],
           }),
+          buildService({ url }) {
+            return new RemoteGraphQLDataSource({
+              url,
+              willSendRequest({ request, context }) {
+                request.http.headers.set(
+                  'user',
+                  context.user ? JSON.stringify(context.user) : null,
+                );
+              },
+            });
+          },
         },
       }),
       inject: [ConfigService],
     }),
+    ClientsModule.registerAsync([
+      {
+        name: AUTH_SERVICE,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configService.getOrThrow('AUTH_HOST'),
+            port: configService.getOrThrow('AUTH_PORT'),
+          },
+        }),
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
